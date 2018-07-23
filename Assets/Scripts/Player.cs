09e37458959dd8521a22e686ba760bf9bbd42310
Gernot Wechselberger <gernot.wechselberger@outlook.com>
2018-07-23 11:41:18 +0200

    Auto Tiles

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        Run();
+	}
+
+    void Run()
+    {
+        float xAxis = CrossPlatformInputManager.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xAxis * runSpeed, rb.velocity.y);
+    }
+}
